07c07cbe426e8750e29ab356e354a3b97d8d09c0
asBrettisay <brett.caudill.is@gmail.com>
2016-07-21 14:25:45 -0600

    modified async prompt.
    Deleted answer.

--- src/assessment.js
+++ src/assessment.js
@@ -1,18 +1,7 @@
 var foo;
 /* Write a function called async.
-  async will return a promise.
-  That promise will call setTimeout on a function
+  Use $q to create a promise object and return the promise.
+  Call setTimeout on a function
   which changes the variable foo (above) to 'bar'
   And resolve the promise when setTimeout completes.
 */
-
-function async() {
-  var defer = $q.defer();
-
-  setTimeout(function() {
-    foo = 'bar';
-    defer.resolve();
-  }, 1000)
-
-  return defer.promise;
-}
